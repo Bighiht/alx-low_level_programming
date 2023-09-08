65657ada0368ba00a148476721c69b5bfe962d00
Bighiht <knightofthemask44@gmail.com>
2023-09-08 08:03:19 -0700

    ffwe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != e && ch != q)
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
