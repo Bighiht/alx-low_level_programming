f10b65891925596ecb1ab2799a2f9d2695e45b2e
Bighiht <knightofthemask44@gmail.com>
2023-09-08 08:30:07 -0700

    fe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{	int n = 0;
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
