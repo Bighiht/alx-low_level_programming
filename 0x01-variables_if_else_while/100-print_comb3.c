17caeeb1400b18033c9f66de5694565a4381a6a7
Bighiht <knightofthemask44@gmail.com>
2023-09-08 10:32:22 -0700

    fe

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{	int n, i;
+	for (n = 0; n <= 8; n++)
+	{
+		for (i = n + 1; i <= 9; i++)
+		{
+			putchar(n + '0');
+			putchar(i + '0');
+			if (n != 8 || i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putcher('\n');
+	return (0);
+}
