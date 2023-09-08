332e5cd93d97c021fa08fe224f22785eab8af941
Bighiht <knightofthemask44@gmail.com>
2023-09-08 06:23:23 -0700

    fe

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char low_alp = 'a';
+	while (low_alp <= 'z')
+	{
+		putchar(low_alp);
+		low_alp++;
+	}
+	putchar('\n');
+}
