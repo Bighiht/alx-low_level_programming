a9bb020c44c9e5d72a81172e38d00499de81ae29
Bighiht <knightofthemask44@gmail.com>
2023-09-08 08:23:57 -0700

    fe

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,11 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-{
-	for (int n = 0; n < 10 ; n++)
+{	int n = 0;
+	while (n < 10)
 	{
 		printf("%d", n);
+		n++;
 	}
 	printf("\n");
 	return (0);
