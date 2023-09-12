71b3dfb69a30028b6ef4cf36ffbf89f2b42a15e3
Bighiht <knightofthemask44@gmail.com>
2023-09-12 06:01:15 -0700

    fa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * @n: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (0);
+}
