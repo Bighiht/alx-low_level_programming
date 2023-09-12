f299d9ad7346526d6b1bd794922de8a9b27b80e1
Bighiht <knightofthemask44@gmail.com>
2023-09-12 05:22:19 -0700

    fa

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 67 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
