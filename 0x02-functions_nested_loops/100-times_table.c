b86892026fcff586488aee8200a0975358e509fe
Bighiht <knightofthemask44@gmail.com>
2023-09-12 16:39:15 -0700

    fe

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ *
+ * @n: aad
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_times_table(int n)
+{	int row, column, multi;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			_putchar('0');
+			for (column = 1; column <= n; column++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				multi = row * column;
+				if (multi <= 9)
+					_putchar(' ');
+				if (multi <= 99)
+					_putchar(' ');
+				if (multi >= 100)
+				{
+					_putchar((multi / 100) + '0');
+					_putchar((multi % 10) + '0');
+				}
+				else if (multi <= 99 && multi >= 10)
+					_putchar((multi / 10) + '0');
+				_putchar((multi % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
