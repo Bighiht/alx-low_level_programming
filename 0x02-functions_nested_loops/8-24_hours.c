5b29ca55f463f524c49a5cbf96c79c36b7314ea6
Bighiht <knightofthemask44@gmail.com>
2023-09-12 07:24:36 -0700

    da

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Return: Always 0 (Success)
+*/
+
+void jack_bauer(void)
+{	int hours, minuts;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minuts = 0; minuts <= 59; minuts++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minuts / 10) + '0');
+			_putchar((minuts / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
