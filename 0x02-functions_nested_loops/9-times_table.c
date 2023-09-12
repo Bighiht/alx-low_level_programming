65cc5d228ed172efdbe88d32207e34cf55b37a95
Bighiht <knightofthemask44@gmail.com>
2023-09-12 14:24:30 -0700

    fq

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{	int row, column, multi;
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+		for (column = 1; column <= 9; column++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			multi = row * column;
+			if (multi <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((multi / 10) + '0');
+			}
+			_putchar((multi % 10) + '0');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
