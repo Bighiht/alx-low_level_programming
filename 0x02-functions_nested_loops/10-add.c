400e4d183ee56c98ee99179f0172e0f0c76fa612
Bighiht <knightofthemask44@gmail.com>
2023-09-12 14:34:50 -0700

    da

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @x: A C program that prints with printf function.
+ *
+ * @y: abu
+ *
+ * Return: Always 0 (Success)
+*/
+
+int add(int x, int y)
+{	int sum = x + y;
+
+	return (sum);
+}
