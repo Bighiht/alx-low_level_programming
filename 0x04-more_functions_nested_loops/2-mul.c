16058e93d4a9a076ce68ad3d9a6c266826612a8e
Bighiht <knightofthemask44@gmail.com>
2023-09-14 04:21:26 -0700

    da

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: A C program that prints with printf function.
+ *
+ * @b: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
