057c1677326daa92b46a17ee155fdb8598927722
Bighiht <knightofthemask44@gmail.com>
2023-09-20 05:31:32 -0700

    da

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * _strcat - Entry point
+ * _strncat - Entry point
  *
  * @dest: A C program that prints with printf function.
  *
  * @src: fwfop
  *
+ * @n: fwse
+ *
  * Return: Always 0 (Success)
 */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {	int i = 0, j;
 
 	while (dest[i])
 		i++;
-	for (j = 0; src[j]; j++)
-		dest[i++] = scr[j];
+	for (j = 0; i < n && scr[j] != '\0'; j++)
+		dest[i + j] = scr[j];
+	dest[i + j] = '\0';
 	return (dest);
 }
